2d4b487da6df943c43d03eeca6d92d4475fe624e
luckytoilet <bai.li.2005@gmail.com>
2016-06-11 00:18:27 +0000

    make scripts directory

--- run_all.sh
+++
@@ -1,4 +0,0 @@
-~/betting/nitrogenspider/run_NA.sh
-~/betting/nitrogenspider/run_EU.sh
-~/betting/nitrogenspider/run_LCK.sh
-
---
+++ scripts/run_all.sh
@@ -0,0 +1,5 @@
+~/betting/nitrogenspider/scripts/run_NA.sh
+~/betting/nitrogenspider/scripts/run_EU.sh
+~/betting/nitrogenspider/scripts/run_LCK.sh
+~/betting/nitrogenspider/scripts/kill_all_ps.sh
+
